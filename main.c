a044fe4c553581f2ea22d7db27762efcd5e568e5
degasys1970 <150461049+degasys1970@users.noreply.github.com>
2023-11-11 20:54:20 +0000

    kész

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  int sum;
+  sum = 0;
+  printf("Kiírom 1000 -ig minden tizedik 3 -mal osztható számot\n\n");
+  for (int i = 0; i < 1000; i++) {
+    if(i % 3 == 0) {
+      sum += i;
+      if(i % 30 == 0) {
+        printf("%d, ", i);
+    }
+    }
+  }
+    printf("\n\n1000-ig a 3 mal osztható számok összege %d ", sum);
   return 0;
 }
